474955ec3c392cb0c6b7f1114a59f27452076342
Research Computing Services <rcs@dhcp-129-105-211-253.library.northwestern.edu>
2017-05-02 16:10:56 -0500

    save cookies between requests

--- initial_mech.py
+++ initial_mech.py
@@ -1,8 +1,7 @@
 __author__ = 'rcs'
-from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
-import urllib2
+
 
 # goto proudct page
 # get product info using bs4 (sku, model#, requestkey, etc..)
@@ -14,69 +13,81 @@ import urllib2
 # with requests.Session() as session:
 
 def main():
-    base_url = "http://www.footlocker.com/"
+
     URL = "http://www.footlocker.com/product/model:266161/"
     sesh = "http://footlocker.com/session"
     # create session
     session = requests.Session()
-    return addToCart(session, URL)
+    return addToCart(URL)
 
-def addToCart(sesh, url):
-    session = sesh
-    r = session.get(url)
-    html = r.text
-    referer = r.url
-    soup = BeautifulSoup(html,"html.parser")
+def addToCart(url):
+    base_url = "http://www.footlocker.com/"
+    req_headers = {
+        'User-Agent': "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1"
+    }
+    resp_headers = {
+        'Accept': '*/*',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Origin': base_url,
+        'Referer': url,
+        'Accept-Encoding': 'gzip, deflate',
+        'User-Agent': "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1"
+    }
+    with requests.Session() as session:
+        r = session.get(url,headers=req_headers)
+        cookies = dict(r.cookies)
+        html = r.text
+        referer = r.url
+        soup = BeautifulSoup(html,"html.parser")
 
-    qty = '1'
-    size = '13.0'
+        qty = '1'
+        size = '13.0'
 
-    #add to cart payload
-    # form id="product_form"
-    # get VALUE
+        #add to cart payload
+        # form id="product_form"
+        # get VALUE
 
-    model = soup.find("input", id='pdp_model')['value'].encode("utf8") # id="pdp_model"
-    name = soup.find("input", id='model_name')['value'].encode("utf8") # id="model_name"
-    requestKey = soup.find("input", id='requestKey')['value'].encode("utf8") # id="requestKey"
-    sku = soup.find("input", id='pdp_selectedSKU')['value'].encode("utf8") # id="pdp_selectedSKU"
-    price = soup.find("input", attrs={'name':'selectedPrice', 'type':'hidden'})['value'].encode("utf8") # id="selectedPrice"
+        model = soup.find("input", id='pdp_model')['value'].encode("utf8") # id="pdp_model"
+        name = soup.find("input", id='model_name')['value'].encode("utf8") # id="model_name"
+        requestKey = soup.find("input", id='requestKey')['value'].encode("utf8") # id="requestKey"
+        sku = soup.find("input", id='pdp_selectedSKU')['value'].encode("utf8") # id="pdp_selectedSKU"
+        price = soup.find("input", attrs={'name':'selectedPrice', 'type':'hidden'})['value'].encode("utf8") # id="selectedPrice"
 
-    load = { # need to set default values/ variables
-             'storeCostOfGoods':'0.00',
-             'lineItemId':'',
-             'model': model,
-             'requestKey': 'F28D6gBB28EE28FE',#requestKey,
-             'hasXYPromo':'false',
-             'sameDayDeliveryConfig':'false',
-             'sku':sku,
-             'the_model_nbr': model,
-             'model_name': name,
-             'skipISA':'no',
-             'selectedPrice': price,
-             'qty': qty,
-             'size':size,
-             'fulfillmentType':'SHIP_TO_HOME',
-             'storeNumber':'00000',
-             'coreMetricsDo':'true',
-             'coreMetricsCategory':'Add to Wish List - PDP',
-             'quantity': qty,
-             'inlineAddToCart':'1'
-             }
-    # headers = {
-    #     'Accept': '*/*',
-    #     'Origin': base_url,
-    #     'X-Requested-With': 'XMLHttpRequest',
-    #     'Referer': referer,
-    #     'Accept-Encoding': 'gzip, deflate',
-    # }
-    print r.request.headers
-    print requestKey
-    response = requests.post('http://www.footlocker.com/catalog/miniAddToCart.cfm?secure=0&', data=load)
-    response_text = BeautifulSoup(response.text, "html.parser")
-    if response.status_code == 500:
-        print "ERROR: {0}".format([err.string.encode("utf8") for err in response_text.find_all("span", class_='error')])
-        # print response.headers
-    else:
-        print response_text
+        load = { # need to set default values/ variables
+                 'storeCostOfGoods':'0.00',
+                 'lineItemId':'',
+                 'model': model,
+                 'requestKey': 'F28D6gBB28EE28FE',#requestKey,
+                 'hasXYPromo':'false',
+                 'sameDayDeliveryConfig':'false',
+                 'sku':sku,
+                 'the_model_nbr': model,
+                 'model_name': name,
+                 'skipISA':'no',
+                 'selectedPrice': price,
+                 'qty': qty,
+                 'size':size,
+                 'fulfillmentType':'SHIP_TO_HOME',
+                 'storeNumber':'00000',
+                 'coreMetricsDo':'true',
+                 'coreMetricsCategory':'Add to Wish List - PDP',
+                 'quantity': qty,
+                 'inlineAddToCart':'1'
+                 }
 
-print main()
+        print requestKey
+        response = session.post('http://www.footlocker.com/catalog/miniAddToCart.cfm?secure=0&', headers=resp_headers, data=load, cookies=cookies)
+        response_text = BeautifulSoup(response.text, "html.parser")
+        print "Status Code: {0}".format(response.status_code)
+        if response.status_code == 500:
+            # print "ERROR: {0}".format([err.string.encode("utf8") for err in response_text.find_all("span", class_='error')])
+            print response.request.headers
+        elif response.status_code == 200:
+            print response_text
+            print 'great success'
+        else:
+            print response.status_code
+            print response_text
+    return
+
+main()
